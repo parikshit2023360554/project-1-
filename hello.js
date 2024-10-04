556ca5a84af22b3d10a9bc30bd36136c75ae58fa
Parikshit Singh <158508938+parikshit2023360554@users.noreply.github.com>
2024-10-04 14:45:14 +0530

    added
    dashboard added to the htm;l

---
+++ hello.js
@@ -0,0 +1,7 @@
+function hide(){
+    let get = document.getElementById('user-info-display');
+    console.log(get);
+    get.style.display = "flex";
+    let getsign = document.getElementById('but');
+    getsign.style.display ='none';
+}
